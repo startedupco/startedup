f9f77163a4c37d2ee23665e45abe08595f565a07
startedup <thehelpdeskcentral@gmail.com>
2026-09-20 00:03:04 +0200

    Add files via upload

--- supabase-client.js
+++ supabase-client.js
@@ -1,3 +1,5 @@
 var SUPABASE_URL = 'https://gzxbppwlfxtbbynxlocw.supabase.co';
 var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6eGJwcHdsZnh0Ynlienhsb2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MjY2NjEsImV4cCI6MjEwMjQwMjY2MX0.rvTEgP-JATqMy2Nx2arreY9ABgEMwCwIlBKRhYMILDs';
-var supabase = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
+var supabase = (window.supabase && typeof window.supabase.createClient === 'function')
+    ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+    : null;
